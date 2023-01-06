bb882007e51f3e625bae0f9e18431e8a6b0c8f73
Mauri <mauri.monzon91@gmail.com>
2023-01-06 00:14:37 -0300

    Hecho test 6,7,8 y 9

--- 09.js
+++ 09.js
@@ -25,8 +25,17 @@ function filtrar(funcion) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
 
-};
+  Array.prototype.filtrar = function (funcion) {
+    let nuevoArreglo = [];
+    for (let i = 0; i < this.length; i++) {
+      if (funcion(this[i])) {
+        nuevoArreglo.push(this[i]);
+      }
+    }
+    return nuevoArreglo;
+  };
+}
 
 // No modifiques nada debajo de esta linea //
 
-module.exports = filtrar
+module.exports = filtrar;
